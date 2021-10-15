9b94d073bd0f77a1723325bf7fede33ca91f5067
kaseoll <82272315+kaseoll@users.noreply.github.com>
2021-10-15 21:57:12 +0800

    Update url1.js

--- js/url1.js
+++ js/url1.js
@@ -1,3 +1,3 @@
-var url = '{
-
-}';
+var UEEngine = '{"url":{"urlname":"虚幻引擎","zh_weburl":"www.unrealengine.com/zh-CN/?lang=zh-CN","urltype":2}}';
+var YandexSearchEngine = '{"url":{"urlname":"Yandex搜索","zh_weburl":"yandex.com/","urltype":2}}';
+var phpStudy = '{"url":{"urlname":"PHPStudy","zh_weburl":"www.xp.cn","urltype":2}}';
